9053c8ee51242395b926149e24442b8cb5a9fa9f
Trev Kelderman <keldermant@district65.net>
2018-11-30 08:36:04 -0600

    Initial Commit

---
+++ DEPNotifyenrollpolicy.sh.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Triggers for the policies to be chained.  Please leave the double quotes around the name of the trigger 
+policyA="depnotifyenroll"
+####################################################################################################
+# Code
+# Do not modify below this line
+####################################################################################################
+#Check to see if this is a new computer record, or has a jamf.log that is greater than 50 lines. 
+old=$(cat /var/log/jamf.log | wc -l | awk '{print$1}')
+if [ "$old" -gt 50 ]; 
+then
+	echo "It looks like this computer has likely been enrolled before.  The jamf.log contains $old lines"
+	exit 0
+else
+#Running DEPNotify enrollment policy:
+echo "Running DEPNotify enrollment Policy...."
+/usr/local/bin/jamf policy -event "$policyA"
+exitCode="$?"
+
+if [ $exitCode == 0 ]; 
+then
+	echo "DEPNotify enrollment policy executed successfully."
+	else
+		echo "First policy failed.  Exit code is $exitCode.."
+		exit $exitCode
+fi
+
+fi
